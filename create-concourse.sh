58145af11eaa0f0e824dc0f22242564a8b17d699
Toshiaki Maki <tmaki@pivotal.io>
2019-12-05 12:20:16 +0900

    Use m4.large

--- create-concourse.sh
+++ create-concourse.sh
@@ -24,18 +24,18 @@ bosh create-env ${CONCOURSE_DEPLOYMENT}/lite/concourse.yml \
   -o <(sed 's|/instance_groups/name=web|/instance_groups/name=concourse|g' ${CONCOURSE_DEPLOYMENT}/cluster/operations/privileged-https.yml) \
   -o ${CONCOURSE_DEPLOYMENT}/cluster/operations/tls-vars.yml \
   -o <(cat <<EOF
-# - type: replace
-#   path: /resource_pools/0/cloud_properties/instance_type
-#   value: m4.large
-# - type: replace
-#   path: /resource_pools/0/cloud_properties/spot_bid_price?
-#   value: 0.0340
 - type: replace
   path: /resource_pools/0/cloud_properties/instance_type
-  value: t2.medium
+  value: m4.large
 - type: replace
   path: /resource_pools/0/cloud_properties/spot_bid_price?
-  value: 0.0190
+  value: 0.0340
+# - type: replace
+#   path: /resource_pools/0/cloud_properties/instance_type
+#   value: t2.medium
+# - type: replace
+#   path: /resource_pools/0/cloud_properties/spot_bid_price?
+#   value: 0.0190
 - type: replace
   path: /resource_pools/0/cloud_properties/spot_ondemand_fallback?
   value: true
